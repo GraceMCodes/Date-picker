06a77c5dba599c4d98f02806ecd4416751dea44f
Grace M codes <gracemcodes@gmail.com>
2024-07-19 02:39:41 +0300

    I added the js.

---
+++ app.js
@@ -0,0 +1,19 @@
+document.querySelector('#show').addEventListener('click',showDate );
+ 
+function showDate(){
+     const text = document.querySelector('#text').value.toLowerCase();
+     if(
+        text == 'tuesday' ||  text == 'thursday'
+     ){
+        console.log(' Go in your zoom!')
+     } else if (
+        text == 'saturday' ||  text == 'sunday'
+     ){
+        console.log ('Free day!')
+     }
+     else{
+        console.log('Submit your homework!')
+     }
+
+
+}
