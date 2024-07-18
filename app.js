84421b4af25a3047534e72b7b8e9dffe4e620476
Grace M codes <gracemcodes@gmail.com>
2024-07-19 02:42:16 +0300

    I corrected my js. I was using  == instead of ===

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-document.querySelector('#show').addEventListener('click',showDate );
+window.document.querySelector('#show').addEventListener('click',showDate );
  
 function showDate(){
      const text = document.querySelector('#text').value.toLowerCase();
      if(
-        text == 'tuesday' ||  text == 'thursday'
+        text === 'tuesday' ||  text === 'thursday'
      ){
         console.log(' Go in your zoom!')
      } else if (
-        text == 'saturday' ||  text == 'sunday'
+        text === 'saturday' ||  text === 'sunday'
      ){
         console.log ('Free day!')
      }
